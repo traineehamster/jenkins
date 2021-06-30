43aadf347d035099a8ebff9bb60b164008b8c38a
Olga Slazarska <o.slazarska@gmail.com>
2021-06-30 17:19:16 +0300

    uncomment tests

--- src/test/java/tests/OwnerTests.java
+++ src/test/java/tests/OwnerTests.java
@@ -7,18 +7,18 @@ import static config.Credentials.credentials;
 
 public class OwnerTests {
 
-//    @Test
-//    @Tag("owner")
-//    void readFromPropertiesTest() {
-//        String login = credentials.login();
-//        String password = credentials.password();
-//
-//        System.out.println(login);
-//        System.out.println(password);
-//
-//
-//        String message = String.format("I try to login as %s with password %s", login, password);
-//
-//        System.out.println(message);
-//    }
+    @Test
+    @Tag("owner")
+    void readFromPropertiesTest() {
+        String login = credentials.login();
+        String password = credentials.password();
+
+        System.out.println(login);
+        System.out.println(password);
+
+
+        String message = String.format("I try to login as %s with password %s", login, password);
+
+        System.out.println(message);
+    }
 }
